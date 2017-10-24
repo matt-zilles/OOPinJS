be3db2f81d2ff315384fab6c4bb1706ae16dabe9
Matthew Zilles <107486@hboe.org>
2017-10-24 13:39:11 -0400

    lmoa

--- sketch.js
+++ sketch.js
@@ -5,22 +5,48 @@ var bubble = {
 
 function setup() { // built-in P5.JS function -=- this runs once
 	createCanvas(600, 400); 
+	bubble1 = new Bubble();
+	bubble2 = new Bubble();
 }
 
 function draw() { // built-in P5.JS function -=-  automatic loop that repeats forever
 	background(0); // give the canvas a black background
-	move();
-	display();
+	bubble1.move();
+	bubble1.show();
+	bubble2.move();
+	bubble2.show();
 }
 
-function display() {
-	stroke(255); // white outline
-	strokeWeight(4); // line width
+class Bubble 
+{
+	constructor(x, y, r) {
+		this.x = 200;
+		this.y = 200;
+		this.r = r;
+}
+
+
+move(){
+	bubble.x = bubble.x + random(-5, 5);
+	bubble.y = bubble.y + random(-5, 5);
+}
+
+show() {
+	stroke(255); 
+	strokeWeight(4); 
 	noFill();
-	ellipse(bubble.x, bubble.y, 24, 24); // draw an ellipse/circle
+	ellipse(this.x, this.y, this.r * 2);
+	
+/*
+move(){
+	bubble.x = bubble.x + random(-5, 5);
+	bubble.y = bubble.y + random(-5, 5);
 }
 
-function move() {
-	bubble.x = bubble.x + random(-5,5);
-	bubble.y = bubble.y + random(-5,5);
-}
+show() {
+	stroke(255); 
+	strokeWeight(4); 
+	noFill();
+	ellipse(bubble.x, bubble.y, 24, 24);
+	*/ 
+	
