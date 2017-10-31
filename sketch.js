739572f1c64f2a9c32df13b5e4c315c6c96cc979
Matthew Zilles <107486@hboe.org>
2017-10-31 13:07:24 -0400

    l

--- sketch.js
+++ sketch.js
@@ -2,52 +2,70 @@ let bubbles = [];
 
 function setup() { // built-in P5.JS function -=- this runs once
 	createCanvas(600, 400); 
-	for (let i = 0; i < 1000; 1++) {
-		let x = random(width);
-		let y = random(height);
-		let x = random(10, 40);
-		bubbles[i] = new Bubble(x, y, r);
+for(let i = 0; 1 < 10; i++) {
+	let x = random(width);
+	let y = random(height);
+	let r = random(10, 50);
+	let b = new Bubble(x, y, r);
+	bubbles.push(b);
+	}
+}
+
+
+function mousePressed() {
+	for (let i = 0; i < bubbles.length; i++) {
+		if(bubbles[i].contains(mouseX, mouseY)) {
+			bubbles.splice(i,1);
+		}
 	}
 }
 
 function draw() { // built-in P5.JS function -=-  automatic loop that repeats forever
 	background(0); // give the canvas a black background
 	for (let i = 0; i < bubbles.length; i++) {
+		if(bubbles[i].contains(mouseX, mouseY)) {
+			bubbles[i].changeColor(255);
+		} else {
+			bubbles[i].changeColor(0);
+		}
 		bubbles[i].move();
 		bubbles[i].show();
 	}
 }
 
-class Bubble 
-{
+class Bubble {
 	constructor(x, y, r) {
 		this.x = x;
 		this.y = y;
 		this.r = r;
+		this.brightness = 0;
+}
+
+changeColor(bright) {
+	this.brightness = bright;
+}
+
+contains(px, py) {
+	let d = dist(px, py, this.x, this.y);
+	if (d < this.r){
+		return true;
+	}else{
+		return false;
+	}
 }
 
 
 move(){
-	bubble.x = bubble.x + random(-5, 5);
-	bubble.y = bubble.y + random(-5, 5);
+	this.x = this.x + random(-5, 5);
+	this.y = this.y + random(-5, 5);
 }
 
 show() {
 	stroke(255); 
 	strokeWeight(4); 
 	noFill();
-	ellipse(this.x, this.y, this.r * 2);
-	
-/*
-move(){
-	bubble.x = bubble.x + random(-5, 5);
-	bubble.y = bubble.y + random(-5, 5);
+	ellipse(this.x, this.y, this.r * 2, this.r * 2);
+}
 }
 
-show() {
-	stroke(255); 
-	strokeWeight(4); 
-	noFill();
-	ellipse(bubble.x, bubble.y, 24, 24);
-	*/ 
-	
+
